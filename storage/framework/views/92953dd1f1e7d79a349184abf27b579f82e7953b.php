bf4a5814f9d895152ddd6c9cbd71a393414be7d0
Miroslav Mašek <masek.mira@gmail.com>
2019-01-15 21:59:53 +0000

    production stuff

--- storage/framework/views/92953dd1f1e7d79a349184abf27b579f82e7953b.php
+++
@@ -1,4 +0,0 @@
-<?php $__env->startSection('content'); ?>
-    <order-detail :current-user="<?php echo e(Auth::user()); ?>" gopay-order-id="<?php echo e($id); ?>"></order-detail>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layout', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
--- storage/framework/views/c77d2a548f1e53e5b36b0d8d0df7315f464f89f3.php
+++
@@ -1,7 +0,0 @@
-<?php echo $__env->make('layouts.partials.header', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
-<v-app id="app">
-    <navigations :current-user="<?php echo e(Auth::user()); ?>"></navigations>
-    <?php echo $__env->yieldContent('content'); ?>
-    <footer-component></footer-component>
-</v-app>
-<?php echo $__env->make('layouts.partials.footer', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
